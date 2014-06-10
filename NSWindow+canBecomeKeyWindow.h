c2cf01fb5fe4de99d33a08b12f9964ec6e831958
Cristiana Yambo <cristiana@cloudthirteen.com>
2014-06-10 15:32:31 -0700

    Added NSWindow+canBecomeKeyWindow category to fix issue where you can’t type in the menubar app

---
+++ NSWindow+canBecomeKeyWindow.h
@@ -0,0 +1,13 @@
+//
+//  NSWindow+canBecomeKeyWindow.h
+//  Password Factory
+//
+//  Created by Cristiana Yambo on 6/10/14.
+//  Copyright (c) 2014 c13. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSWindow (canBecomeKeyWindow)
+
+@end
