4525b8fc1a4986ba5d6385c05b1ee9355dfafbea
Cristiana Yambo <cristiana.yambo@gmail.com>
2015-08-16 15:45:58 -0400

    re-added MainWindow

---
+++ Password Factory/MainWindow.h
@@ -0,0 +1,13 @@
+//
+//  MainWindow.h
+//  Password Factory
+//
+//  Created by Cristiana Yambo on 8/16/15.
+//  Copyright (c) 2015 Cristiana Yambo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainWindow : NSWindow
+
+@end
---
+++ Password Factory/MainWindow.m
@@ -0,0 +1,18 @@
+//
+//  MainWindow.m
+//  Password Factory
+//
+//  Created by Cristiana Yambo on 8/16/15.
+//  Copyright (c) 2015 Cristiana Yambo. All rights reserved.
+//
+
+#import "MainWindow.h"
+
+@implementation MainWindow
+-(BOOL)canBecomeKeyWindow {
+    return YES;
+}
+-(BOOL)canBecomeMainWindow {
+    return YES;
+}
+@end
