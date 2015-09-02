2cb1c6f555d5f609201b94d71b04be260833163e
Cristiana Yambo <cristiana.yambo@gmail.com>
2015-09-02 13:39:09 -0400

    widget mostly working started adding 'leetify' controls

---
+++ Password Factory/Categories/NSString+SymbolCase.h
@@ -0,0 +1,13 @@
+//
+//  NSString+SymbolCase.h
+//  Password Factory
+//
+//  Created by Cristiana Yambo on 9/2/15.
+//  Copyright (c) 2015 Cristiana Yambo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (SymbolCase)
+
+@end
---
+++ Password Factory/Categories/NSString+SymbolCase.m
@@ -0,0 +1,13 @@
+//
+//  NSString+SymbolCase.m
+//  Password Factory
+//
+//  Created by Cristiana Yambo on 9/2/15.
+//  Copyright (c) 2015 Cristiana Yambo. All rights reserved.
+//
+
+#import "NSString+SymbolCase.h"
+
+@implementation NSString (SymbolCase)
+
+@end
