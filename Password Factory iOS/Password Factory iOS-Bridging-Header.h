f4ea43a92af3021ef93cb7d958eb3d222ebbca47
Cristiana Yambo <cristiana.yambo@gmail.com>
2015-12-04 15:01:44 -0800

    bridging header

---
+++ Password Factory iOS/Password Factory iOS-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  PasswordFactory-Bridging-Header.h
+//  Password Factory
+//
+//  Created by Cristiana Yambo on 12/4/15.
+//  Copyright © 2015 Cristiana Yambo. All rights reserved.
+//
+
+#ifndef PasswordFactory_Bridging_Header_h
+#define PasswordFactory_Bridging_Header_h
+
+
+#endif /* PasswordFactory_Bridging_Header_h */
+
+#import "PasswordFactory.h"
