ba5b56bde92fc7abeef620882f90338cd1fc3f58
Cristiana Yambo <cristiana.yambo@gmail.com>
2015-12-04 12:39:58 -0800

    fixing ios

---
+++ Password Factory iOS/RandomViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RandomViewController.swift
+//  Password Factory
+//
+//  Created by Cristiana Yambo on 12/4/15.
+//  Copyright © 2015 Cristiana Yambo. All rights reserved.
+//
+
+import Foundation
