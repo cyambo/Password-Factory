c15c82484e5e96edf32b7e4342a5fd1bd8637a19
Cristiana Yambo <cristiana.yambo@gmail.com>
2017-11-26 20:42:56 -0500

    Preferences constraints working

---
+++ Password Factory/Controls/BoxView.h
@@ -0,0 +1,13 @@
+//
+//  BoxView.h
+//  Password Factory
+//
+//  Created by Cristiana Yambo on 11/26/17.
+//  Copyright © 2017 Cristiana Yambo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BoxView : NSView
+
+@end
---
+++ Password Factory/Controls/BoxView.m
@@ -0,0 +1,19 @@
+//
+//  BoxView.m
+//  Password Factory
+//
+//  Created by Cristiana Yambo on 11/26/17.
+//  Copyright © 2017 Cristiana Yambo. All rights reserved.
+//
+
+#import "BoxView.h"
+
+@implementation BoxView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
