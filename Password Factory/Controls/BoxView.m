a94b92fa91ca5e1897b8c1d81dd4c81a8881f76f
Cristiana Yambo <cristiana.yambo@gmail.com>
2017-11-26 21:31:25 -0500

    aligned everything in preferences

--- Password Factory/Controls/BoxView.m
+++ Password Factory/Controls/BoxView.m
@@ -22,19 +22,23 @@
     [self setWantsLayer:YES];
     self.layer.backgroundColor = [[NSColor colorWithWhite:0.0 alpha:0.02] CGColor];
     self.layer.cornerRadius = 10.0;
+    self.layer.borderColor = [[NSColor colorWithWhite:0.0 alpha:0.03] CGColor];
+    self.layer.borderWidth = 0.5;
     self.titleText = [[NSTextField alloc] init];
     [self addSubview:self.titleText];
     self.titleText.backgroundColor = [NSColor clearColor];
     self.titleText.bordered = NO;
     self.titleText.selectable = NO;
-    [self.titleText setAlphaValue:0.3];
+    [self.titleText setAlphaValue:0.4];
     [self.titleText setFont:[NSFont systemFontOfSize:10]];
+    [self.titleText setAlignment:NSTextAlignmentCenter];
     return self;
 }
 -(void)viewWillDraw {
     [self.titleText setStringValue:self.boxTitle];
     CGFloat yvalue = self.frame.size.height - 15;
-    NSRect frame = NSMakeRect(11,yvalue, 240, 14);
+    CGFloat width = self.frame.size.width - 30;
+    NSRect frame = NSMakeRect(15,yvalue, width, 14);
     [self.titleText setFrame:frame];
     
 }
