1ccb941815a827f3ad368ccea6dee88f5a1f6b00
Cristiana Yambo <cristiana.yambo@gmail.com>
2018-02-02 15:15:29 -0500

    updated help

--- Password Factory/Assets/PasswordFactory.help/Contents/Resources/en.lproj/scrpt/main.js
+++ Password Factory/Assets/PasswordFactory.help/Contents/Resources/en.lproj/scrpt/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    window.addEventListener("hashchange", function() { scrollBy(0, -70) })
+    window.addEventListener("hashchange", function() { scrollBy(0, -70) });
+     var navMain = $(".navbar-collapse"); // avoid dependency on #id
+     // "a:not([data-toggle])" - to avoid issues caused
+     // when you have dropdown inside navbar
+     navMain.on("click", "a:not([data-toggle])", null, function () {
+         navMain.collapse('hide');
+     });
+
 });
